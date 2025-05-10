7c2d79fa94ff5020d6bf5aada8b1c782082de964
Ridwan Mugdha <ridwaniqbalmugdha@gmail.com>
2025-05-10 18:24:07 +0200

    Compact Module

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p class="text-6xl text-amber-300 text-center">
+        Tailwind and react is working Bold and Yellow
+      </p>
+    </>
+  );
+}
+
+export default App;

09a98d1baa726eb17b44ed03ca7ce5fe9d588099
Ridwan Mugdha <ridwaniqbalmugdha@gmail.com>
2025-05-10 18:32:49 +0200

    Compact Module 1

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import "./App.css";
 function App() {
   return (
     <>
-      <p class="text-6xl text-amber-300 text-center">
-        Tailwind and react is working Bold and Yellow
+      <p class="text-6xl text-red-300 text-center">
+        Tailwind and react is working Bold and red
       </p>
     </>
   );
